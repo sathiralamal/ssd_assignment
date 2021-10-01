fc344bfdf759255500e13ba7c33c418ee728fcf5
VidulaDakshitha <viduladakshitha@gmail.com>
2021-10-01 21:52:35 +0530

    made changes

--- server.js
+++ server.js
@@ -1,46 +1,138 @@
-const express = require('express')
-const app = express()
-const core = require("cors");
-const bodyParser = require("body-parser");
-const nHGet = require("./app");
-const port = 3002
-app.use(core());
-app.use(bodyParser());
-app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+const fs = require("fs");
+const express = require("express");
+const multer = require("multer");
+const OAuth2Data = require("./client_secret.json");
+var name,pic
 
-app.get("/upload",function (req, res) {
-    try {
+const { google } = require("googleapis");
 
-        //var urls = req.body.url;
-        var urls=["https://images.unsplash.com/photo-1633096613998-42001089ba89?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=686&q=80"]
-        nHGet.Save(urls, (err) => {
-            if (err)
-            {
-                
-                console.log("error adding")
-                
-            } else{
-                console.log("no error")
-                
-            }
-            //res.send(200)
-        }).then((val)=>{
-            res.send("success")
-        }).catch((err)=>{
-            res.send("error")
-        })
-       
+const app = express();
 
 
-    } catch (e) {
-        console.log("errot" + e);
-    }
+const CLIENT_ID = OAuth2Data.web.client_id;
+const CLIENT_SECRET = OAuth2Data.web.client_secret;
+const REDIRECT_URL = OAuth2Data.web.redirect_uris[0];
+
+const oAuth2Client = new google.auth.OAuth2(
+  CLIENT_ID,
+  CLIENT_SECRET,
+  REDIRECT_URL
+);
+var authed = false;
+
+// If modifying these scopes, delete token.json.
+const SCOPES =
+  "https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/userinfo.profile";
+
+app.set("view engine", "ejs");
+
+var Storage = multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, "./images");
+  },
+  filename: function (req, file, callback) {
+    callback(null, file.fieldname + "_" + Date.now() + "_" + file.originalname);
+  },
 });
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+var upload = multer({
+  storage: Storage,
+}).single("file"); //Field name and max count
+
+app.get("/", (req, res) => {
+  if (!authed) {
+    // Generate an OAuth URL and redirect there
+    var url = oAuth2Client.generateAuthUrl({
+      access_type: "offline",
+      scope: SCOPES,
+    });
+    console.log(url);
+    res.render("index", { url: url });
+  } else {
+    var oauth2 = google.oauth2({
+      auth: oAuth2Client,
+      version: "v2",
+    });
+    oauth2.userinfo.get(function (err, response) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(response.data);
+        name = response.data.name
+        pic = response.data.picture
+        res.render("success", {
+          name: response.data.name,
+          pic: response.data.picture,
+          success:false
+        });
+      }
+    });
+  }
+});
+
+app.post("/upload", (req, res) => {
+  upload(req, res, function (err) {
+    if (err) {
+      console.log(err);
+      return res.end("Something went wrong");
+    } else {
+      console.log(req.file.path);
+      const drive = google.drive({ version: "v3",auth:oAuth2Client  });
+      const fileMetadata = {
+        name: req.file.filename,
+      };
+      const media = {
+        mimeType: req.file.mimetype,
+        body: fs.createReadStream(req.file.path),
+      };
+      drive.files.create(
+        {
+          resource: fileMetadata,
+          media: media,
+          fields: "id",
+        },
+        (err, file) => {
+          if (err) {
+            // Handle error
+            console.error(err);
+          } else {
+            fs.unlinkSync(req.file.path)
+            res.render("success",{name:name,pic:pic,success:true})
+          }
+
+        }
+      );
+    }
+  });
+});
+
+app.get('/logout',(req,res) => {
+    authed = false
+    res.redirect('/')
 })
+
+app.get("/google/callback", function (req, res) {
+  const code = req.query.code;
+  if (code) {
+    // Get an access token based on our OAuth code
+    oAuth2Client.getToken(code, function (err, tokens) {
+      if (err) {
+        console.log("Error authenticating");
+        console.log(err);
+      } else {
+        console.log("Successfully authenticated");
+        console.log(tokens)
+        oAuth2Client.setCredentials(tokens);
+
+
+        authed = true;
+        res.redirect("/");
+      }
+    });
+  }
+});
+
+app.listen(5000, () => {
+  console.log("App is listening on Port 5000");
+});
